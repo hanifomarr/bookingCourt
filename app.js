49cceabf3e6dcb8e1055398d14ec72148e7f27e1
hanifomarr <57039713+hanifomarr@users.noreply.github.com>
2023-03-23 17:02:26 +0800

    AppError class and async catch utils function

--- app.js
+++ app.js
@@ -4,6 +4,8 @@ const mongoose = require("mongoose");
 const ejsMate = require("ejs-mate");
 const Venue = require("./models/venue");
 const methodOverride = require("method-override");
+const AppError = require("./utils/AppError");
+const CatchAsync = require("./utils/CatchAsync");
 
 mongoose.connect("mongodb://127.0.0.1:27017/vortax");
 
@@ -25,43 +27,75 @@ app.get("/", (req, res) => {
   res.render("home");
 });
 
-app.get("/venue", async (req, res) => {
-  const venues = await Venue.find({});
-  res.render("venues/index", { venues });
-});
+app.get(
+  "/venue",
+  CatchAsync(async (req, res) => {
+    const venues = await Venue.find({});
+    res.render("venues/index", { venues });
+  })
+);
 
 app.get("/venue/new", (req, res) => {
   res.render("venues/new");
 });
 
-app.post("/venue", async (req, res) => {
-  const newVenue = new Venue(req.body.venue);
-  await newVenue.save();
-  res.redirect(`/venue/${newVenue._id}`);
+app.post(
+  "/venue",
+  CatchAsync(async (req, res, next) => {
+    if (!req.body.venue) {
+      throw new AppError("Invalid Data", 400);
+    }
+    const newVenue = new Venue(req.body.venue);
+    await newVenue.save();
+    res.redirect(`/venue/${newVenue._id}`);
+  })
+);
+
+app.get(
+  "/venue/:id",
+  CatchAsync(async (req, res) => {
+    const { id } = req.params;
+    const venue = await Venue.findById(id);
+    res.render("venues/show", { venue });
+  })
+);
+
+app.get(
+  "/venue/:id/edit",
+  CatchAsync(async (req, res) => {
+    const { id } = req.params;
+    const venue = await Venue.findById(id);
+    res.render("venues/edit", { venue });
+  })
+);
+
+app.put(
+  "/venue/:id",
+  CatchAsync(async (req, res) => {
+    const { id } = req.params;
+    const updateVenue = await Venue.findByIdAndUpdate(id, {
+      ...req.body.venue,
+    });
+    res.redirect(`/venue/${updateVenue._id}`);
+  })
+);
+
+app.delete(
+  "/venue/:id",
+  CatchAsync(async (req, res) => {
+    const { id } = req.params;
+    await Venue.findByIdAndDelete(id);
+    res.redirect("/venue");
+  })
+);
+
+app.all("*", (req, res, next) => {
+  next(new AppError("Not Found", 404));
 });
 
-app.get("/venue/:id", async (req, res) => {
-  const { id } = req.params;
-  const venue = await Venue.findById(id);
-  res.render("venues/show", { venue });
-});
-
-app.get("/venue/:id/edit", async (req, res) => {
-  const { id } = req.params;
-  const venue = await Venue.findById(id);
-  res.render("venues/edit", { venue });
-});
-
-app.put("/venue/:id", async (req, res) => {
-  const { id } = req.params;
-  const updateVenue = await Venue.findByIdAndUpdate(id, { ...req.body.venue });
-  res.redirect(`/venue/${updateVenue._id}`);
-});
-
-app.delete("/venue/:id", async (req, res) => {
-  const { id } = req.params;
-  await Venue.findByIdAndDelete(id);
-  res.redirect("/venue");
+app.use((err, req, res, next) => {
+  const { status = 500, message = "Something Wrong" } = err;
+  res.status(status).send(message);
 });
 
 app.listen(3000, () => {
